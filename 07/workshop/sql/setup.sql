5fa00b84b9a78bc1b3f633b74eb83bd87d678456
kaur-harman <amharrman@gmail.com>
2026-03-17 00:17:22 +0530

    update flink dockerfile and create tables

---
+++ 07/workshop/sql/setup.sql
@@ -0,0 +1,12 @@
+-- CREATE TABLE q4_results (
+--     window_start TIMESTAMP,
+--     pulocationid INT,
+--     num_trips BIGINT,
+--     PRIMARY KEY (window_start, pulocationid)
+-- );
+
+CREATE TABLE q5_results (
+    pulocationid INT,
+    num_trips BIGINT
+);
+
